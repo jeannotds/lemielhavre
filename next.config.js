12f76a603977181cd0ee76f7a2b00033339e44ac
Jeannot Diambu <diambujeannot@gmail.com>
2026-01-05 20:00:06 +0100

    fix : error

--- next.config.js
+++ next.config.js
@@ -1,23 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: { unoptimized: true },
-// };
-
-// module.exports = nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.cache = false;
-    }
-    return config;
+  eslint: {
+    ignoreDuringBuilds: true,
   },
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
-
